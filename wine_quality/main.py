9b03d2bc19f2007b21c7d74c797128a4903f57d3
shadman_git <khezri.shadman@gmail.com>
2024-02-20 23:10:43 +0330

    make train and test data

--- wine_quality/main.py
+++ wine_quality/main.py
@@ -1,4 +1,8 @@
 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
 from src.display import display
 from src.read_file import read_file
 from src.show_info_data import show_info_data
@@ -32,7 +36,20 @@ def main():
 
     display(quality_value_counts(df_wine))
 
-    show_plot_quality(df_wine["quality"])
+    # show_plot_quality(df_wine["quality"])
+
+
+    X = df_wine.drop('quality', axis = 1)
+    y = df_wine['quality']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+    sc = StandardScaler()
+
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.fit_transform(X_test)
+
+
 
 
 if __name__ == "__main__":
